0d1adca94e609ebe567480534ad3062a1c328faa
monbear79 <yenok457@gmail.com>
2023-08-09 14:31:28 +0700

    first commit

---
+++ fe/src/components/FooterComponent/FooterComponent.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function FooterComponent() {
+  return (
+    <footer style={{ backgroundColor: '#e5e5e5', padding: '20px', textAlign: 'center' }}>
+      This is the footer. Place your footer content here.Xin chào, mình là Sunny đến từ Girls' Generation.
+Mình có một câu chuyện muốn nói nên mình mới viết ra những dòng này. Trước tiên mình muốn gửi lời cảm ơn đến mọi người nhân dịp 16 năm SNSD ra mắt.
+Nhìn lại chặng đường đã qua, mình biết ơn rất nhiều người trong hơn 16 năm qua. Những thành viên SNSD quý giá như chính bản thân mình vậy, họ là gia đình thân yêu của mình,
+các staff đã âm thầm ở bên cạnh mình, và những người hâm mộ đã cho mình sức mạnh chỉ bằng cách đơn giản nhất vẫn luôn ở đó..
+Hôm nay, mình muốn bày tỏ lòng biết ơn và lời chúc mừng tới SM Entertainment, công ty đã giúp Lee Soon- gyu, 19 tuổi, ra mắt với tư cách là Sunny của Girls' Generation.
+Hơn hết mình muốn cảm ơn cả những người quản lý đã làm việc chăm chỉ cùng nhau mình trong suốt 16 năm hoạt động giải trí và tất cả các nhân viên đã giúp mình truyền năng lượng tích cực đến nhiều người thông qua các sân khấu, nội dung âm nhạc hay và cả một tạo hình đẹp trên sân khấu.
+Được ra mắt với tư cách là "Girls' Generation" và gặp gỡ các thành viên cũng như "Sowon" mỗi ngày khiến mình hiểu hạnh phúc không thể đo đếm được.
+Bây giờ mình lấy hết can đảm để nhìn nhận bản thân ở một khía cạnh khác trong một môi trường mới. Đó là nhờ sự quan tâm và tình cảm thủy chung của người hâm mộ. Bất kể chuyện gì sẽ đến, chúng mình sẽ tiếp tục tiến về phía trước với hình ảnh đầy nắng là nguồn năng lượng. Mình sẽ trở thành một Sunny chia sẻ những điều tốt đẹp trong tương lai. 
+Xin trân trọng cảm ơn.
+    </footer>
+  );
+}
+
+export default FooterComponent;
